929b246628636028f40005edea871c453ec43fdf
Anatoli Beliaev <anatolib@microsoft.com>
2020-10-11 00:27:45 -0700

    Install PowerShell 7

--- tools/Crank/Agent/Windows/bootstrap.ps1
+++ tools/Crank/Agent/Windows/bootstrap.ps1
@@ -1,1 +1,7 @@
-throw 'bootstrap.ps1: not implemented yet!'
+# Install PowerShell 7
+$powerShellVersion = '7.0.3'
+$powerShellMsiName = "PowerShell-$powerShellVersion-win-x64.msi"
+[Net.ServicePointManager]::SecurityProtocol =[Net.SecurityProtocolType]::Tls12
+Invoke-RestMethod -Uri "https://github.com/PowerShell/PowerShell/releases/download/v$powerShellVersion/$powerShellMsiName" -OutFile $powerShellMsiName
+& msiexec.exe /package $powerShellMsiName /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 | echo Waiting
+
