de2a136fb3621672ab339498160b889506e81588
Anatoli Beliaev <anatolib@microsoft.com>
2020-10-11 15:24:24 -0700

    Revert "TEMP: Just trace from bootstrap.ps1"
    This reverts commit 28336d7d87eb4204f72521666a9701ff641464d1.

--- tools/Crank/Agent/Windows/bootstrap.ps1
+++ tools/Crank/Agent/Windows/bootstrap.ps1
@@ -1,4 +1,12 @@
-Write-Host 'Hello from bootstrap.ps1 (Host)'
-Write-Verbose 'Hello from bootstrap.ps1 (Verbose)' -Verbose
-Write-Output 'Hello from bootstrap.ps1 (Output)'
-Write-Warning 'Hello from bootstrap.ps1 (Warning)'
+param($ParametersJson)
+
+mkdir C:\github
+Set-Location C:\github
+git clone https://github.com/Azure/azure-functions-host.git
+Set-Location azure-functions-host
+git checkout dev
+
+$powerShellCmd = Windows\install-powershell.ps1
+
+Set-Location tools\Crank\Agent
+& $powerShellCmd -File .\setup-crank-agent-json.ps1 -ParametersJson $ParametersJson -Verbose
