37bd66c849e16557428e7a421d8c7a581f22ffdd
Anatoli Beliaev <anatolib@microsoft.com>
2020-10-11 11:25:57 -0700

    Fix paths

--- tools/Crank/Agent/Windows/bootstrap.ps1
+++ tools/Crank/Agent/Windows/bootstrap.ps1
@@ -1,12 +1,12 @@
 param($ParametersJson)
 
-mkdir ~/github
-Set-Location ~/github
+mkdir C:\github
+Set-Location C:\github
 git clone https://github.com/Azure/azure-functions-host.git
 Set-Location azure-functions-host
 git checkout dev
 
-$powerShellCmd = Windows/install-powershell.ps1
+$powerShellCmd = Windows\install-powershell.ps1
 
-Set-Location tools/Crank/Agent
-& $powerShellCmd -File ./setup-crank-agent-json.ps1 -ParametersJson $ParametersJson -Verbose
+Set-Location tools\Crank\Agent
+& $powerShellCmd -File .\setup-crank-agent-json.ps1 -ParametersJson $ParametersJson -Verbose
