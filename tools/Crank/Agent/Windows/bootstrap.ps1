a11f514fe03e9ef3e67cdeb10f113e50aed678a0
Anatoli Beliaev <anatolib@microsoft.com>
2020-10-11 00:46:54 -0700

    Get azure-functions-host from GitHub

--- tools/Crank/Agent/Windows/bootstrap.ps1
+++ tools/Crank/Agent/Windows/bootstrap.ps1
@@ -1,7 +1,10 @@
-# Install PowerShell 7
-$powerShellVersion = '7.0.3'
-$powerShellMsiName = "PowerShell-$powerShellVersion-win-x64.msi"
-[Net.ServicePointManager]::SecurityProtocol =[Net.SecurityProtocolType]::Tls12
-Invoke-RestMethod -Uri "https://github.com/PowerShell/PowerShell/releases/download/v$powerShellVersion/$powerShellMsiName" -OutFile $powerShellMsiName
-& msiexec.exe /package $powerShellMsiName /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 | echo Waiting
+param($ParametersJson)
+
+mkdir ~/github
+Set-Location ~/github
+git clone https://github.com/Azure/azure-functions-host.git
+Set-Location azure-functions-host
+git checkout dev
+
+$powerShellCmd = Windows/install-powershell.ps1
 
