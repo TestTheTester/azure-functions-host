2f51714300e18dbf85b472ed0c081681729f0885
Anatoli Beliaev <anatolib@microsoft.com>
2020-10-11 17:05:22 -0700

    Inline install-git.ps1 into bootstrap.ps1

--- tools/Crank/Agent/Windows/bootstrap.ps1
+++ tools/Crank/Agent/Windows/bootstrap.ps1
@@ -1,12 +1,25 @@
 param($ParametersJson)
 
+$ErrorActionPreference = 'Stop'
+
+# Install git
+$gitVersion = '2.28.0'
+$gitInstallerFileName = "git-$gitVersion-64-bit.exe"
+[Net.ServicePointManager]::SecurityProtocol =[Net.SecurityProtocolType]::Tls12
+Invoke-RestMethod -Uri "https://github.com/git-for-windows/git/releases/download/v$gitVersion.windows.1/$gitInstallerFileName" -OutFile $gitInstallerFileName
+& ".\$gitInstallerFileName" /SP- /VERYSILENT /NORESTART /SUPPRESSMSGBOXES /ALLUSERS /NORESTART /FORCECLOSEAPPLICATIONS | echo Waiting
+$gitCommand = "$env:ProgramFiles\Git\cmd\git.exe"
+
+# Clone azure-functions-host repo
 mkdir C:\github
 Set-Location C:\github
-git clone https://github.com/Azure/azure-functions-host.git
+& $gitCommand clone https://github.com/Azure/azure-functions-host.git
 Set-Location azure-functions-host
-git checkout dev
+& $gitCommand checkout dev
 
+# Install PowerShell
 $powerShellCmd = Windows\install-powershell.ps1
 
+# Setup Crank agent
 Set-Location tools\Crank\Agent
 & $powerShellCmd -File .\setup-crank-agent-json.ps1 -ParametersJson $ParametersJson -Verbose
--- tools/Crank/Agent/Windows/install-git.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-$gitVersion = '2.28.0'
-$gitInstallerFileName = "git-$gitVersion-64-bit.exe"
-[Net.ServicePointManager]::SecurityProtocol =[Net.SecurityProtocolType]::Tls12
-Invoke-RestMethod -Uri "https://github.com/git-for-windows/git/releases/download/v$gitVersion.windows.1/$gitInstallerFileName" -OutFile $gitInstallerFileName
-& ".\$gitInstallerFileName" /SP- /VERYSILENT /NORESTART /SUPPRESSMSGBOXES /ALLUSERS /NORESTART /FORCECLOSEAPPLICATIONS | echo Waiting
-
-"$env:ProgramFiles\Git\cmd\git.exe"
