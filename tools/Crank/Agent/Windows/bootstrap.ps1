4b7c3ab55e9747cb5e96ced8c6a0e49bd5290c65
Anatoli Beliaev <anatolib@microsoft.com>
2020-10-12 00:31:53 -0700

    Add git to PATH

--- tools/Crank/Agent/Windows/bootstrap.ps1
+++ tools/Crank/Agent/Windows/bootstrap.ps1
@@ -8,14 +8,14 @@ $gitInstallerFileName = "git-$gitVersion-64-bit.exe"
 [Net.ServicePointManager]::SecurityProtocol =[Net.SecurityProtocolType]::Tls12
 Invoke-RestMethod -Uri "https://github.com/git-for-windows/git/releases/download/v$gitVersion.windows.1/$gitInstallerFileName" -OutFile $gitInstallerFileName
 & ".\$gitInstallerFileName" /SP- /VERYSILENT /NORESTART /SUPPRESSMSGBOXES /ALLUSERS /NORESTART /FORCECLOSEAPPLICATIONS | echo Waiting
-$gitCommand = "$env:ProgramFiles\Git\cmd\git.exe"
+$env:PATH += ";$($env:ProgramFiles)\Git\cmd;"
 
 # Clone azure-functions-host repo
 New-Item -Path C:\github -ItemType Directory
 Set-Location -Path C:\github
-& $gitCommand clone https://github.com/Azure/azure-functions-host.git
+& git clone https://github.com/Azure/azure-functions-host.git
 Set-Location -Path azure-functions-host
-& $gitCommand checkout anatolib/crank-windows-vm-deployment
+& git checkout anatolib/crank-windows-vm-deployment
 
 # Install PowerShell
 Set-Location -Path tools\Crank\Agent
